910878b41b03887b1abb475e2db2c33c4fabd27e
Wojciech Marek <marek.wojciech@outlook.com>
2024-03-04 13:12:42 +0100

    chore: format the vhdl program

--- program.vhd
+++ program.vhd
@@ -1,17 +1,17 @@
-library ieee;
-use ieee.std_logic_1164.all;
- 
-entity example_and is
-  port (
-    input_1    : in  std_logic;
-    input_2    : in  std_logic;
-    and_result : out std_logic
-    );
-end example_and;
- 
-architecture rtl of example_and is
-  signal and_gate : std_logic;
-begin
-  and_gate   <= input_1 and input_2;
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+
+ENTITY example_and IS
+  PORT (
+    input_1 : IN STD_LOGIC;
+    input_2 : IN STD_LOGIC;
+    and_result : OUT STD_LOGIC
+  );
+END example_and;
+
+ARCHITECTURE rtl OF example_and IS
+  SIGNAL and_gate : STD_LOGIC;
+BEGIN
+  and_gate <= input_1 AND input_2;
   and_result <= and_gate;
-end rtl;
+END rtl;
