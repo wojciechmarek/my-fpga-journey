72ba60e660aa9df3719b7c4c4911d6d0180d51eb
Wojciech Marek <marek.wojciech@outlook.com>
2024-03-26 18:11:12 +0100

    feat: add logic-gates examples

---
+++ logic-gates/gates/or-gate-with-input-and-output-negation.vhdl
@@ -0,0 +1,17 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+
+ENTITY example_gate IS
+    PORT (
+        button_a : IN STD_LOGIC;
+        button_b : IN STD_LOGIC;
+        led : OUT STD_LOGIC
+    );
+END example_gate;
+
+ARCHITECTURE rtl OF example_gate IS
+    SIGNAL gate_result : STD_LOGIC;
+BEGIN
+    gate_result <= (NOT button_a) OR (NOT button_b);
+    led <= NOT gate_result;
+END rtl;
